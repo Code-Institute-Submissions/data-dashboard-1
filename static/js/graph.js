59d0feeb833263277b9093f13b4388455a123fa1
Noelle Browne <noelle.browne@yahoo.ie>
2018-11-07 16:13:25 +0000

    added a header to the file and alse a graph.js script

--- static/js/graph.js
+++ static/js/graph.js
@@ -2,6 +2,7 @@ queue()
     .defer(d3.csv, "data/googleplaystore.csv")
     .await(makeGraph);
     
-function makeGraph(error, storeData) {
-     
-}    
+function makeGraph(error, googleData) {
+
+    
+}
