077761d1a8928ea74af04ad7e09504e0bcbc8b06
Noelle Browne <noelle.browne@yahoo.ie>
2018-11-16 16:52:24 +0000

    added parsedates to graph.js and added heading to index.html

--- static/js/graph.js
+++ static/js/graph.js
@@ -2,55 +2,65 @@ queue()
 	.defer(d3.csv, "data/bakery.csv")
 	.await(makegraphs);
 
-
-function makegraphs(error, bakeryData) {
-
+function makegraphs(error, bakeryData){
 	var ndx = crossfilter(bakeryData);
+
+	var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+	 bakeryData.forEach(function(d) {
+		d.date = parseDate(d.date);
+		
+	 });
+
+function forEach(error, parseDate) {
+	var ndx = crossfilter(ndx);
 }
 
-var bakeryData = [
-	2016 - 10 - 30, 09: 58: 11, 1, Bread
-	2016 - 10 - 30, 10: 05: 34, 2, Scandinavian
-	2016 - 10 - 30, 10: 05: 34, 2, Scandinavian
-	2016 - 10 - 30, 10: 07: 57, 3, Hot chocolate
-	2016 - 10 - 30, 10: 07: 57, 3, Jam
-	2016 - 10 - 30, 10: 07: 57, 3, Cookie
-	2016 - 10 - 30, 10: 08: 41, 4, Muffin
-	2016 - 10 - 30, 10: 13: 03, 5, Coffee
-	2016 - 10 - 30, 10: 13: 03, 5, Pastry
-	2016 - 10 - 30, 10: 13: 03, 5, Bread
-	2016 - 10 - 30, 10: 16: 55, 6, Medialuna
-	2016 - 10 - 30, 10: 16: 55, 6, Pastry
-	2016 - 10 - 30, 10: 16: 55, 6, Muffin
-	2016 - 10 - 30, 10: 19: 12, 7, Medialuna
-	2016 - 10 - 30, 10: 19: 12, 7, Pastry
-	2016 - 10 - 30, 10: 19: 12, 7, Coffee
-	2016 - 10 - 30, 10: 19: 12, 7, Tea
-	2016 - 10 - 30, 10: 20: 51, 8, Pastry
-	2016 - 10 - 30, 10: 20: 51, 8, Bread
-	2016 - 10 - 30, 10: 21: 59, 9, Bread
-	2016 - 10 - 30, 10: 21: 59, 9, Muffin
-];
+	var barChart = dc.barChart("#transaction-data");
+    var pieChart = dc.pieChart("#item");
+    var lineChart = dc.lineChart("#purchase-date-sales");
+
+
+ 
+
+// var bakeryData = [
+// 	"2016 - 10 - 30 09: 58: 11", "1", "Bread",
+// 	"2016 - 10 - 30 10: 05: 34", "2", "Scandinavian",
+// 	"2016 - 10 - 30 10: 05: 34", "2", "Scandinavian",
+// 	"2016 - 10 - 30 10: 07: 57", "3", "Hot chocolate",
+// 	"2016 - 10 - 30 10: 07: 57", "3", "Jam",
+// 	"2016 - 10 - 30 10: 07: 57", "3", "Cookie",
+// 	"2016 - 10 - 30 10: 08: 41", "4", "Muffin",
+// 	"2016 - 10 - 30 10: 13: 03", "5", "Coffee",
+// 	"2016 - 10 - 30 10: 13: 03", "5", "Pastry",
+// 	"2016 - 10 - 30 10: 13: 03", "5", "Bread",
+// 	"2016 - 10 - 30 10: 16: 55", "6", "Medialuna",
+// 	"2016 - 10 - 30 10: 16: 55", "6", "Pastry",
+// 	"2016 - 10 - 30 10: 16: 55", "6", "Muffin",
+// 	"2016 - 10 - 30 10: 19: 12", "7", "Medialuna",
+// 	"2016 - 10 - 30 10: 19: 12", "7", "Pastry",
+// 	"2016 - 10 - 30 10: 19: 12", "7", "Coffee",
+// 	"2016 - 10 - 30 10: 19: 12", "7", "Tea",
+// 	"2016 - 10 - 30 10: 20: 51", "8", "Pastry",
+// 	"2016 - 10 - 30 10: 20: 51", "8", "Bread",
+// 	"2016 - 10 - 30 10: 21: 59", "9", "Bread",
+// 	"2016 - 10 - 30 10: 21: 59", "9", "Muffin",
+// ];
 
 
 show_transaction_data(ndx);
 show_item_data(ndx);
 show_purchase_time_sales(ndx);
 
+}
 
 function show_transaction_data(ndx) {
 	var transaction_dim = ndx.dimension(dc.pluck('transaction'));
 	var total_transaction_of_sales = transaction_dim.group().reduceSum(dc.pluck('transaction'));
 
-	dc.barchart("#transaction-data")
+	dc.barChart("#transaction-data")
 		.width(300)
 		.height(150)
-		.margins({
-			top: 10,
-			right: 50,
-			bottom: 30,
-			left: 50
-		})
+		.margins({top: 10, right: 50, bottom: 30, left: 50})
 		.dimension(transaction_dim)
 		.group(total_transaction_of_sales)
 		.transitionDuration(500)
@@ -71,15 +81,10 @@ function show_item_data(ndx) {
 		.transitionDuration(1500)
 		.dimension(item_dim)
 		.group(total_item_of_sales);
-
 }
 
-function show_purchase_date_sales(ndx) {
 
-	var parseDate = d3.time.format("%d/%m/%Y").parse;
-	bakeryData.forEach(function (d) {
-		d.date = parseDate(d.date);
-	})
+function show_purchase_time_sales(ndx) {
 
 	var date_dim = ndx.dimension(dc.pluck('date'));
 	var total_date_of_sales = date_dim.group().reduceSum(dc.pluck('date'));
@@ -90,20 +95,15 @@ function show_purchase_date_sales(ndx) {
 	dc.lineChart("#purchase-date-sales")
 		.width(1000)
 		.height(300)
-		.margins({
-			top: 10,
-			right: 50,
-			bottom: 30,
-			left: 50
-		})
+		.margins({top: 10, right: 50, bottom: 30, left: 50})
 		.dimension(date_dim)
 		.group(total_date_of_sales)
 		.transitionDuration(500)
 		.x(d3.time.scale().domain([minDate, maxDate]))
 		.xAxisLabel("Date of items purchased")
 		.yAxis().ticks(4);
-}
+		
+	dc.renderAll();
+		
 
-dc.renderAll();
-
-};
+	}
