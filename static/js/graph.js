b5372125969230fa9dc2c40ebfb1a1cc55fdef93
Noelle Browne <noelle.browne@yahoo.ie>
2019-01-16 15:31:54 +0000

    added pechart and barchart

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,21 +1,23 @@
-queue()
-	.defer(d3.json, "data/bakery.json")
-	.await(makegraphs);
-
-function makegraphs(error, bakeryData){
-	var ndx = crossfilter(bakeryData);
-
-	show_transaction_data(ndx);
-	show_item_data(ndx);
-
-    dc.renderAll();
+queue()   
+   .defer(d3.json, "data/bakery.json")
+   .await(makeGraphs);
+    
+function makeGraphs(error, bakeryData) {
+        var ndx = crossfilter(bakeryData);
+       
+        show_transaction_data(ndx);
+        show_item_data(ndx);
+       
+        
+        dc.renderAll();
 
 }
 
-function show_transaction_data(ndx) {
 
-	var transaction_dim = ndx.dimension(dc.pluck('item'));
-	var total_transaction_of_sales = transaction_dim.group().reduceSum(dc.pluck('transaction'));
+function show_transaction_data(ndx) {
+	
+	var transaction_dim = ndx.dimension(dc.pluck('food'));
+	var total_transaction_of_sales = transaction_dim.group().reduceSum(dc.pluck('sold'));
 
 
 	dc.barChart("#transaction-data")
@@ -28,16 +30,15 @@ function show_transaction_data(ndx) {
 		.x(d3.scale.ordinal())
 		.xUnits(dc.units.ordinal)
 		.xAxisLabel("Transaction")
-		.yAxis().ticks(20);
+		.yAxis().ticks(05);
 
 }
-
 function show_item_data(ndx) {
-	var item_dim = ndx.dimension(dc.pluck('item'));
-	var total_item_of_sales = item_dim.group().reduceSum(dc.pluck('transaction'));
+	var item_dim = ndx.dimension(dc.pluck('food'));
+	var total_item_of_sales = item_dim.group().reduceSum(dc.pluck('sold'));
 	dc.pieChart("#item-data")
-		.height(1000)
-		.radius(450)
+		.height(400)
+		.radius(90)
 		.transitionDuration(1500)
 		.dimension(item_dim)
 		.group(total_item_of_sales);
