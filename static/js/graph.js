f48a80f21dd764c781d195e7cd7c2b4b875a0559
Noelle Browne <noelle.browne@yahoo.ie>
2019-02-02 18:31:54 +0000

    added stacked chart and composite

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,8 +9,8 @@ function makeGraphs(error, fifaData) {
         show_age(ndx); // Piechart 1
         show_position_of_player(ndx); //Piehcart 2
         show_wage(ndx); //Linechart
-      //  overall_vs_potential(ndx); ///COMPOSITE CHART
-      // show_stacked_chart(ndx); ///Stacked CHART        
+        overall_vs_potential(ndx); ///COMPOSITE CHART
+        show_stacked_chart(ndx); ///Stacked CHART        
         dc.renderAll();
 
 }
@@ -76,7 +76,7 @@ function show_position_of_player(ndx) {
         .radius(90)
         .transitionDuration(1500)
         .dimension(position_dim)
-        .group(position_dim.group())
+        .group(position_dim.group());
 
 }
 
@@ -87,9 +87,9 @@ function show_wage(ndx) {
     var width = document.getElementById("wage").offsetWidth;
     var wageDim = ndx.dimension(function(d) {return d.Name;});
     var anotherGroup = wageDim.group().reduceSum(function(d) {
-        let total = parseInt(d.Wage.replace(/[^\d]/g,''),10)
+        let total = parseInt(d.Wage.replace(/[^\d]/g,''),10);
         return total;
-    })
+    });
     dc.lineChart('#wage')
         .width(width)
         .height(300)
@@ -112,73 +112,88 @@ function show_wage(ndx) {
  
 ///COMPOSITE CHART
 
-function overall_vs_potential(ndx) {
-
-    var overall_dim = ndx.dimension(dc.pluck('Overall'));
-
-
-    function overall_vs_potential(name) {
-        return function(d) {
-            if (d.name === name) {
+function makeGraphs(error, transactionsData) {
+        var ndx = crossfilter(transactionsData);
+        var parseDate = d3.time.format("%d/%m/%Y").parse;
+        transactionsData.forEach(function(d){
+            d.date = parseDate(d.date);
+        });
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+        var tomSpendByMonth = date_dim.group().reduceSum(function (d) {
+                if (d.name === 'Tom') {
+                    return +d.spend;
+                } else {
+                    return 0;
+                }
+            });
+        var bobSpendByMonth = date_dim.group().reduceSum(function (d) {
+            if (d.name === 'Bob') {
                 return +d.spend;
-          } else {
-               return 0;
-           }
-       };
+            } else {
+                return 0;
+            }
+        });
+        var aliceSpendByMonth = date_dim.group().reduceSum(function (d) {
+            if (d.name === 'Alice') {
+                return +d.spend;
+            } else {
+                return 0;
+            }
+        });
+        var compositeChart = dc.compositeChart('#chart-here');
+        compositeChart
+            .width(990)
+            .height(200)
+            .dimension(date_dim)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .yAxisLabel("Spend")
+            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .compose([
+                dc.lineChart(compositeChart)
+                    .colors('green')
+                    .group(tomSpendByMonth, 'Tom'),
+                dc.lineChart(compositeChart)
+                    .colors('red')
+                    .group(bobSpendByMonth, 'Bob'),
+                dc.lineChart(compositeChart)
+                    .colors('blue')
+                    .group(aliceSpendByMonth, 'Alice')
+            ])
+            .brushOn(false)
+            .render();
+        dc.renderAll();
     }
-    
-    var overall = overall_dim.group().reduceSum(overall_vs_potential('Potential'));
-    var potential = overall_dim.group().reduceSum(overall_vs_potential('Overall'));
-
-    var compositeChart = dc.compositeChart('#overall-vs-potential');
-
-    compositeChart
-        .width(990)
-        .height(200)
-        .dimension(overall_dim)
-        .x(d3.scale.ordinal())
-        .yAxisLabel("Overall")
-        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-        .renderHorizontalGridLines(true)
-        .compose([
-            dc.lineChart(compositeChart)
-            .colors('green')
-            .group(overall, 'Potential'),
-            dc.lineChart(compositeChart)
-            .colors('blue')
-            .group(potential, 'overall'),
-
-        ])
-        .brushOn(false);
-}
 ///Stacked CHART
 
-function show_stacked_chart(ndx) {
-
-    var name_dim = ndx.dimension(dc.pluck('Name'));
-    var spendByDay1 = name_dim.group().reduceSum(function(d) {
-        if (d.store === '1') {
-            return +d.spend;
-        } else {
-            return 0;
-            }
-    });
-    var spendByDay2 = name_dim.group().reduceSum(function(d) {
-        if (d.store === '2') {
-            return +d.spend;
-        } else {
-            return 0;
-        }
-    });
-    var stackedChart = dc.barChart("#stacked-chart");
-    stackedChart
-        .width(500)
-        .height(500)
-        .dimension(name_dim)
-        .group(spendByDay1, "Preferred Foot")
-        .stack(spendByDay2, "Weak Foot")
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
-    stackedChart.margins().right = 100;
-  }
+    function makeGraphs(error, transactionsData) {
+        var ndx = crossfilter(transactionsData);
+        var name_dim = ndx.dimension(dc.pluck('name'));
+        var spendByNameStoreA = name_dim.group().reduceSum(function (d) {
+                if (d.store === 'A') {
+                    return +d.spend;
+                } else {
+                    return 0;
+                }
+            });
+        var spendByNameStoreB = name_dim.group().reduceSum(function (d) {
+                if (d.store === 'B') {
+                    return +d.spend;
+                } else {
+                    return 0;
+                }
+            });
+        var stackedChart = dc.barChart("#chart-here");
+        stackedChart
+            .width(500)
+            .height(500)
+            .dimension(name_dim)
+            .group(spendByNameStoreA, "Store A")
+            .stack(spendByNameStoreB, "Store B")
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
+        stackedChart.margins().right = 100;
+    }
