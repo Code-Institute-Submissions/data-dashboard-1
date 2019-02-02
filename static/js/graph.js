51b864e283b924eb6ebe060896f214312f9ab0f5
Noelle Browne <noelle.browne@yahoo.ie>
2019-02-02 21:08:06 +0000

    beautified code

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,32 +1,31 @@
-queue()   
-   .defer(d3.json, "data/fifa.json")
-   .await(makeGraphs);
-    
-function makeGraphs(error, fifaData) {
-        var ndx = crossfilter(fifaData);
+queue()
+    .defer(d3.json, "data/fifa.json")
+    .await(makeGraphs);
 
-        show_nationality(ndx); // Barchart
-        show_age(ndx); // Piechart 1
-        show_position_of_player(ndx); //Piehcart 2
-        show_wage(ndx); //Linechart
-        overall_vs_potential(ndx); ///COMPOSITE CHART
-        show_stacked_chart(ndx); ///Stacked CHART        
-        dc.renderAll();
+function makeGraphs(error, fifaData) {
+    var ndx = crossfilter(fifaData);
+
+    show_nationality(ndx); // Barchart
+    show_age(ndx); // Piechart 1
+    show_position_of_player(ndx); //Piehcart 2
+    show_wage(ndx); //Linechart
+    /// overall_vs_potential(ndx); ///COMPOSITE CHART
+    /// show_stacked_chart(ndx); ///Stacked CHART        
+    dc.renderAll();
 
 }
 
-	
 
 
-   
+
 // BARCHART 
 
 function show_nationality(ndx) {
 
     var nationality_dim = ndx.dimension(dc.pluck('Nationality'));
     var total_nationality_of_footballers = nationality_dim.group();
-    
-    
+
+
 
     var width = document.getElementById("nationality").offsetWidth;
 
@@ -53,10 +52,10 @@ function show_nationality(ndx) {
 
 
 function show_age(ndx) {
-	
+
     var age_dim = ndx.dimension(dc.pluck('Name'));
     var total_age_of_players = age_dim.group().reduceSum(dc.pluck('Age'));
-    
+
     dc.pieChart('#age-of-players')
         .height(400)
         .radius(90)
@@ -68,7 +67,7 @@ function show_age(ndx) {
 /// PIECHART 2
 
 function show_position_of_player(ndx) {
-	
+
     var position_dim = ndx.dimension(dc.pluck('Position'));
 
     dc.pieChart('#position-of-player')
@@ -85,9 +84,11 @@ function show_position_of_player(ndx) {
 function show_wage(ndx) {
 
     var width = document.getElementById("wage").offsetWidth;
-    var wageDim = ndx.dimension(function(d) {return d.Name;});
+    var wageDim = ndx.dimension(function(d) {
+        return d.Name;
+    });
     var anotherGroup = wageDim.group().reduceSum(function(d) {
-        let total = parseInt(d.Wage.replace(/[^\d]/g,''),10);
+        let total = parseInt(d.Wage.replace(/[^\d]/g, ''), 10);
         return total;
     });
     dc.lineChart('#wage')
@@ -109,91 +110,91 @@ function show_wage(ndx) {
 
 }
 
- 
+
 ///COMPOSITE CHART
 
-function makeGraphs(error, transactionsData) {
-        var ndx = crossfilter(transactionsData);
-        var parseDate = d3.time.format("%d/%m/%Y").parse;
-        transactionsData.forEach(function(d){
-            d.date = parseDate(d.date);
-        });
-        var date_dim = ndx.dimension(dc.pluck('date'));
-        var minDate = date_dim.bottom(1)[0].date;
-        var maxDate = date_dim.top(1)[0].date;
-        var tomSpendByMonth = date_dim.group().reduceSum(function (d) {
-                if (d.name === 'Tom') {
-                    return +d.spend;
-                } else {
-                    return 0;
-                }
-            });
-        var bobSpendByMonth = date_dim.group().reduceSum(function (d) {
-            if (d.name === 'Bob') {
-                return +d.spend;
-            } else {
-                return 0;
-            }
-        });
-        var aliceSpendByMonth = date_dim.group().reduceSum(function (d) {
-            if (d.name === 'Alice') {
-                return +d.spend;
-            } else {
-                return 0;
-            }
-        });
-        var compositeChart = dc.compositeChart('#chart-here');
-        compositeChart
-            .width(990)
-            .height(200)
-            .dimension(date_dim)
-            .x(d3.time.scale().domain([minDate, maxDate]))
-            .yAxisLabel("Spend")
-            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-            .renderHorizontalGridLines(true)
-            .compose([
-                dc.lineChart(compositeChart)
-                    .colors('green')
-                    .group(tomSpendByMonth, 'Tom'),
-                dc.lineChart(compositeChart)
-                    .colors('red')
-                    .group(bobSpendByMonth, 'Bob'),
-                dc.lineChart(compositeChart)
-                    .colors('blue')
-                    .group(aliceSpendByMonth, 'Alice')
-            ])
-            .brushOn(false)
-            .render();
-        dc.renderAll();
-    }
+///function makeGraphs(error, transactionsData) {
+///     var ndx = crossfilter(transactionsData);
+///    var parseDate = d3.time.format("%d/%m/%Y").parse;
+///     transactionsData.forEach(function(d){
+///         d.date = parseDate(d.date);
+///     });
+///     var date_dim = ndx.dimension(dc.pluck('date'));
+///    var minDate = date_dim.bottom(1)[0].date;
+///    var maxDate = date_dim.top(1)[0].date;
+///    var tomSpendByMonth = date_dim.group().reduceSum(function (d) {
+///          if (d.name === 'Tom') {
+///              return +d.spend;
+///          } else {
+///             return 0;
+///          }
+///       });
+///    var bobSpendByMonth = date_dim.group().reduceSum(function (d) {
+///        if (d.name === 'Bob') {
+///             return +d.spend;
+///        } else {
+///            return 0;
+///        }
+///     });
+///    var aliceSpendByMonth = date_dim.group().reduceSum(function (d) {
+///         if (d.name === 'Alice') {
+///             return +d.spend;
+///         } else {
+///           return 0;
+///        }
+///      });
+///       var compositeChart = dc.compositeChart('#chart-here');
+///       compositeChart
+///      .width(990)
+///      .height(200)
+///      .dimension(date_dim)
+///      .x(d3.time.scale().domain([minDate, maxDate]))
+///     .yAxisLabel("Spend")
+///     .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+///     .renderHorizontalGridLines(true)
+///     .compose([
+///          dc.lineChart(compositeChart)
+///              .colors('green')
+///              .group(tomSpendByMonth, 'Tom'),
+///          dc.lineChart(compositeChart)
+///               .colors('red')
+///               .group(bobSpendByMonth, 'Bob'),
+///           dc.lineChart(compositeChart)
+///               .colors('blue')
+///               .group(aliceSpendByMonth, 'Alice')
+///        ])
+///        .brushOn(false)
+///        .render();
+///     dc.renderAll();
+///  }
 ///Stacked CHART
 
-    function makeGraphs(error, transactionsData) {
-        var ndx = crossfilter(transactionsData);
-        var name_dim = ndx.dimension(dc.pluck('name'));
-        var spendByNameStoreA = name_dim.group().reduceSum(function (d) {
-                if (d.store === 'A') {
-                    return +d.spend;
-                } else {
-                    return 0;
-                }
-            });
-        var spendByNameStoreB = name_dim.group().reduceSum(function (d) {
-                if (d.store === 'B') {
-                    return +d.spend;
-                } else {
-                    return 0;
-                }
-            });
-        var stackedChart = dc.barChart("#chart-here");
-        stackedChart
-            .width(500)
-            .height(500)
-            .dimension(name_dim)
-            .group(spendByNameStoreA, "Store A")
-            .stack(spendByNameStoreB, "Store B")
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
-        stackedChart.margins().right = 100;
-    }
+///  function makeGraphs(error, fifaData) {
+///     var ndx = crossfilter(fifData);
+///    var name_dim = ndx.dimension(dc.pluck('name'));
+///   var spendByNameStoreA = name_dim.group().reduceSum(function (d) {
+///          if (d.store === 'A') {
+///              return +d.spend;
+///       } else {
+///           return 0;
+///      }
+///     });
+///   var spendByNameStoreB = name_dim.group().reduceSum(function (d) {
+///        if (d.store === 'B') {
+///          return +d.spend;
+///    } else {
+///        return 0;
+///        }
+///         });
+///    var stackedChart = dc.barChart("#stacked-chart");
+///     stackedChart
+///       .width(500)
+///       .height(500)
+///       .dimension(name_dim)
+///       .group(spendByNameStoreA, "Store A")
+///       .stack(spendByNameStoreB, "Store B")
+///      .x(d3.scale.ordinal())
+///       .xUnits(dc.units.ordinal)
+///       .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
+///   stackedChart.margins().right = 100;
+//}
