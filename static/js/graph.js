7e7b756d895f22ca806850e3263f133174a2ab4c
Noelle Browne <noelle.browne@yahoo.ie>
2019-02-03 22:41:59 +0000

    fixeded stack graph as it wasnt working as i left out code  and also fixed composite the same happened there, both graphs are running now and also are reseting using the reset button which wasnt working.

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,12 +5,12 @@ queue()
 function makeGraphs(error, fifaData) {
     var ndx = crossfilter(fifaData);
 
-    show_nationality(ndx); // Barchart
-    show_age(ndx); // Piechart 1
-    show_position_of_player(ndx); //Piehcart 2
-    show_wage(ndx); //Linechart
-    //show_overall-vs-potential(ndx); ///COMPOSITE CHART
-    show_stacked_chart(ndx); ///Stacked CHART        
+    show_nationality(ndx);
+    show_age(ndx);
+    show_position_of_player(ndx); 
+    show_wage(ndx); 
+    show_overall_vs_potential(ndx);
+    show_stacked_chart(ndx);        
     dc.renderAll();
 
 }
@@ -109,7 +109,71 @@ function show_wage(ndx) {
         .group(anotherGroup);
 
 }
+// composite chart
 
+function show_overall_vs_potential(ndx) {
+  var overall_dim = ndx.dimension(dc.pluck("Name"));
+  var overall = overall_dim.group().reduceSum(d => {
+    console.log("Overall", d.Overall);
+    return d.Overall;
+  });
+  var potential = overall_dim.group().reduceSum(d => {
+    console.log("Potential", d.Potential);
+    return d.Potential;
+  });
+
+  console.log(
+    overall_dim.top(Infinity).map(function(d) {
+      return d.Name;
+    })
+  );
+
+  var compositeChart = dc.compositeChart("#overall-vs-potential");
+
+  compositeChart
+    .width(990)
+    .height(400)
+    .x(d3.scale.ordinal())
+    .y(d3.scale.linear().domain([80, 100]))
+    .xUnits(dc.units.ordinal)
+    .group(overall)
+    .dimension(overall_dim)
+    .elasticX(true)
+    .elasticY(false)
+    .yAxisLabel("Overall vs Potential")
+    .xAxisLabel("Player")
+    .legend(
+      dc
+        .legend()
+        .x(100)
+        .y(20)
+        .itemHeight(13)
+        .gap(5)
+    )
+    .valueAccessor(function(d) {
+      return d.value;
+    })
+    .renderHorizontalGridLines(true)
+    .compose([
+      dc
+        .lineChart(compositeChart)
+        .dimension(overall_dim)
+        .colors("green")
+        .group(overall, "Overall")
+        .dashStyle([2, 2])
+        .renderLabel(true),
+
+      dc
+        .lineChart(compositeChart)
+        .dimension(overall_dim)
+        .colors("red")
+        .group(potential, "Potential")
+        .dashStyle([5, 5])
+        .renderLabel(true)
+    ])
+    .brushOn(false);
+}
+///Stacked CHART
 
 function show_stacked_chart(ndx) {
   var name_dim = ndx.dimension(dc.pluck("Club"));
@@ -139,7 +203,35 @@ function show_stacked_chart(ndx) {
   stackedChart.margins().right = 100;
 }
 
-// Utils
+function show_stacked_chart(ndx) {
+  var name_dim = ndx.dimension(dc.pluck("Club"));
+  var releaseGroup = name_dim
+    .group()
+    .reduceSum(d => parseCurrency(d, "Release Clause"));
+  var valueGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Value"));
+  var wageGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Wage"));
+  var stackedChart = dc.barChart("#stacked-chart");
+  stackedChart
+    .width(990)
+    .height(500)
+    .dimension(name_dim)
+    .group(releaseGroup, "Release Clause")
+    .stack(valueGroup, "Value")
+    .stack(wageGroup, "Wage")
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .legend(
+      dc
+        .legend()
+        .x(50)
+        .y(0)
+        .itemHeight(15)
+        .gap(5)
+    );
+  stackedChart.margins().right = 100;
+}
+
+// Wage 
 function parseCurrency(d, variable = "Wage") {
   const value = d[variable].replace(/€/, "");
   let multiplier = 0;
