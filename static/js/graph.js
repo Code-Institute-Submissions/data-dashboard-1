0a7c189571ae4b5e972c1b59938276ba14263c2b
Noelle Browne <noelle.browne@yahoo.ie>
2019-02-03 22:39:00 +0000

    added a reset button and text to all of my charts for information

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,32 +1,31 @@
-queue()   
-   .defer(d3.json, "data/fifa.json")
-   .await(makeGraphs);
-    
-function makeGraphs(error, fifaData) {
-        var ndx = crossfilter(fifaData);
+queue()
+    .defer(d3.json, "data/fifa.json")
+    .await(makeGraphs);
 
-        show_nationality(ndx); // Barchart
-        show_age(ndx); // Piechart 1
-        show_position_of_player(ndx); //Piehcart 2
-        show_wage(ndx); //Linechart
-      //  overall_vs_potential(ndx); ///COMPOSITE CHART
-      // show_stacked_chart(ndx); ///Stacked CHART        
-        dc.renderAll();
+function makeGraphs(error, fifaData) {
+    var ndx = crossfilter(fifaData);
+
+    show_nationality(ndx); // Barchart
+    show_age(ndx); // Piechart 1
+    show_position_of_player(ndx); //Piehcart 2
+    show_wage(ndx); //Linechart
+    //show_overall-vs-potential(ndx); ///COMPOSITE CHART
+    show_stacked_chart(ndx); ///Stacked CHART        
+    dc.renderAll();
 
 }
 
-	
 
 
-   
+
 // BARCHART 
 
 function show_nationality(ndx) {
 
     var nationality_dim = ndx.dimension(dc.pluck('Nationality'));
     var total_nationality_of_footballers = nationality_dim.group();
-    
-    
+
+
 
     var width = document.getElementById("nationality").offsetWidth;
 
@@ -53,10 +52,10 @@ function show_nationality(ndx) {
 
 
 function show_age(ndx) {
-	
+
     var age_dim = ndx.dimension(dc.pluck('Name'));
     var total_age_of_players = age_dim.group().reduceSum(dc.pluck('Age'));
-    
+
     dc.pieChart('#age-of-players')
         .height(400)
         .radius(90)
@@ -68,7 +67,7 @@ function show_age(ndx) {
 /// PIECHART 2
 
 function show_position_of_player(ndx) {
-	
+
     var position_dim = ndx.dimension(dc.pluck('Position'));
 
     dc.pieChart('#position-of-player')
@@ -76,7 +75,7 @@ function show_position_of_player(ndx) {
         .radius(90)
         .transitionDuration(1500)
         .dimension(position_dim)
-        .group(position_dim.group())
+        .group(position_dim.group());
 
 }
 
@@ -85,11 +84,13 @@ function show_position_of_player(ndx) {
 function show_wage(ndx) {
 
     var width = document.getElementById("wage").offsetWidth;
-    var wageDim = ndx.dimension(function(d) {return d.Name;});
+    var wageDim = ndx.dimension(function(d) {
+        return d.Name;
+    });
     var anotherGroup = wageDim.group().reduceSum(function(d) {
-        let total = parseInt(d.Wage.replace(/[^\d]/g,''),10)
+        let total = parseInt(d.Wage.replace(/[^\d]/g, ''), 10);
         return total;
-    })
+    });
     dc.lineChart('#wage')
         .width(width)
         .height(300)
@@ -109,76 +110,49 @@ function show_wage(ndx) {
 
 }
 
- 
-///COMPOSITE CHART
-
-function overall_vs_potential(ndx) {
-
-    var overall_dim = ndx.dimension(dc.pluck('Overall'));
-
-
-    function overall_vs_potential(name) {
-        return function(d) {
-            if (d.name === name) {
-                return +d.spend;
-          } else {
-               return 0;
-           }
-       };
-    }
-    
-    var overall = overall_dim.group().reduceSum(overall_vs_potential('Potential'));
-    var potential = overall_dim.group().reduceSum(overall_vs_potential('Overall'));
-
-    var compositeChart = dc.compositeChart('#overall-vs-potential');
-
-    compositeChart
-        .width(990)
-        .height(200)
-        .dimension(overall_dim)
-        .x(d3.scale.ordinal())
-        .yAxisLabel("Overall")
-        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-        .renderHorizontalGridLines(true)
-        .compose([
-            dc.lineChart(compositeChart)
-            .colors('green')
-            .group(overall, 'Potential'),
-            dc.lineChart(compositeChart)
-            .colors('blue')
-            .group(potential, 'overall'),
-
-        ])
-        .brushOn(false);
-}
-///Stacked CHART
 
 function show_stacked_chart(ndx) {
+  var name_dim = ndx.dimension(dc.pluck("Club"));
+  var releaseGroup = name_dim
+    .group()
+    .reduceSum(d => parseCurrency(d, "Release Clause"));
+  var valueGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Value"));
+  var wageGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Wage"));
+  var stackedChart = dc.barChart("#stacked-chart");
+  stackedChart
+    .width(990)
+    .height(500)
+    .dimension(name_dim)
+    .group(releaseGroup, "Release Clause")
+    .stack(valueGroup, "Value")
+    .stack(wageGroup, "Wage")
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .legend(
+      dc
+        .legend()
+        .x(50)
+        .y(0)
+        .itemHeight(15)
+        .gap(5)
+    );
+  stackedChart.margins().right = 100;
+}
 
-    var name_dim = ndx.dimension(dc.pluck('Name'));
-    var spendByDay1 = name_dim.group().reduceSum(function(d) {
-        if (d.store === '1') {
-            return +d.spend;
-        } else {
-            return 0;
-            }
-    });
-    var spendByDay2 = name_dim.group().reduceSum(function(d) {
-        if (d.store === '2') {
-            return +d.spend;
-        } else {
-            return 0;
-        }
-    });
-    var stackedChart = dc.barChart("#stacked-chart");
-    stackedChart
-        .width(500)
-        .height(500)
-        .dimension(name_dim)
-        .group(spendByDay1, "Preferred Foot")
-        .stack(spendByDay2, "Weak Foot")
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
-    stackedChart.margins().right = 100;
+// Utils
+function parseCurrency(d, variable = "Wage") {
+  const value = d[variable].replace(/€/, "");
+  let multiplier = 0;
+  if (value.includes("M")) {
+    multiplier = 1000000;
   }
+  if (value.includes("K")) {
+    multiplier = 1000;
+  }
+  if (variable == "Wage") {
+    multiplier = multiplier * 52;
+  }
+  const number = parseFloat(value.replace(/(M|K)/g, ""));
+  console.log(d.Name, variable, number * multiplier);
+  return number * multiplier;
+}
