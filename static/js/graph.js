74d438ecc1197e3379a9eea10d3d368b83483147
Noelle Browne <noelle.browne@yahoo.ie>
2019-02-04 23:26:14 +0000

    added a legend to the piecharts and also made them bigger and realigned the composite x axis graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,117 +1,102 @@
 queue()
-    .defer(d3.json, "data/fifa.json")
-    .await(makeGraphs);
+  .defer(d3.json, "data/fifa.json")
+  .await(makeGraphs);
 
 function makeGraphs(error, fifaData) {
-    var ndx = crossfilter(fifaData);
+  var ndx = crossfilter(fifaData);
 
-    show_nationality(ndx);
-    show_age(ndx);
-    show_position_of_player(ndx); 
-    show_wage(ndx); 
-    show_overall_vs_potential(ndx);
-    show_stacked_chart(ndx);        
-    dc.renderAll();
-
+  show_nationality(ndx); // Barchart
+  show_age(ndx); // Piechart 1
+  show_position_of_player(ndx); //Piehcart 2
+  show_wage(ndx); //Linechart
+  overall_vs_potential(ndx, fifaData); ///COMPOSITE CHART
+  show_stacked_chart(ndx); ///Stacked CHART
+  dc.renderAll();
 }
 
-
-
-
-// BARCHART 
+// BARCHART
 
 function show_nationality(ndx) {
+  var nationality_dim = ndx.dimension(dc.pluck("Nationality"));
+  var total_nationality_of_footballers = nationality_dim.group();
 
-    var nationality_dim = ndx.dimension(dc.pluck('Nationality'));
-    var total_nationality_of_footballers = nationality_dim.group();
+  var width = document.getElementById("nationality").offsetWidth;
 
-
-
-    var width = document.getElementById("nationality").offsetWidth;
-
-
-    dc.barChart("#nationality")
-        .width(width)
-        .height(300)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 30,
-            left: 50
-        })
-        .dimension(nationality_dim)
-        .group(total_nationality_of_footballers)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Players Nationality")
-        .yAxis().ticks(10);
-};
+  dc.barChart("#nationality")
+    .width(width)
+    .height(300)
+    .margins({
+      top: 10,
+      right: 50,
+      bottom: 30,
+      left: 50
+    })
+    .dimension(nationality_dim)
+    .group(total_nationality_of_footballers)
+    .transitionDuration(500)
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .xAxisLabel("Players Nationality")
+    .yAxis()
+    .ticks(10);
+}
 
 /// PIECHART 1
 
-
 function show_age(ndx) {
+  var age_dim = ndx.dimension(dc.pluck("Name"));
+  var total_age_of_players = age_dim.group().reduceSum(dc.pluck("Age"));
 
-    var age_dim = ndx.dimension(dc.pluck('Name'));
-    var total_age_of_players = age_dim.group().reduceSum(dc.pluck('Age'));
-
-    dc.pieChart('#age-of-players')
-        .height(400)
-        .radius(90)
-        .transitionDuration(1500)
-        .dimension(age_dim)
-        .group(total_age_of_players);
+  dc.pieChart("#age-of-players")
+    .height(400)
+    .radius(90)
+    .transitionDuration(1500)
+    .dimension(age_dim)
+    .group(total_age_of_players);
 }
 
 /// PIECHART 2
 
 function show_position_of_player(ndx) {
+  var position_dim = ndx.dimension(dc.pluck("Position"));
 
-    var position_dim = ndx.dimension(dc.pluck('Position'));
-
-    dc.pieChart('#position-of-player')
-        .height(400)
-        .radius(90)
-        .transitionDuration(1500)
-        .dimension(position_dim)
-        .group(position_dim.group());
-
+  dc.pieChart("#position-of-player")
+    .height(400)
+    .radius(90)
+    .transitionDuration(1500)
+    .dimension(position_dim)
+    .group(position_dim.group());
 }
 
 /// LINECHART
 
 function show_wage(ndx) {
+  var width = document.getElementById("wage").offsetWidth;
+  var wageDim = ndx.dimension(dc.pluck("Name"));
+  var anotherGroup = wageDim.group().reduceSum(parseCurrency);
 
-    var width = document.getElementById("wage").offsetWidth;
-    var wageDim = ndx.dimension(function(d) {
-        return d.Name;
-    });
-    var anotherGroup = wageDim.group().reduceSum(function(d) {
-        let total = parseInt(d.Wage.replace(/[^\d]/g, ''), 10);
-        return total;
-    });
-    dc.lineChart('#wage')
-        .width(width)
-        .height(300)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 30,
-            left: 50
-        })
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .brushOn(false)
-        .xAxisLabel('Player')
-        .yAxisLabel('Wage')
-        .dimension(wageDim)
-        .group(anotherGroup);
-
+  console.log("WAGE", anotherGroup);
+  dc.lineChart("#wage")
+    .width(width)
+    .height(300)
+    .margins({
+      top: 10,
+      right: 50,
+      bottom: 30,
+      left: 50
+    })
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .brushOn(false)
+    .xAxisLabel("Player")
+    .yAxisLabel("Wage")
+    .dimension(wageDim)
+    .group(anotherGroup);
 }
-// composite chart
 
-function show_overall_vs_potential(ndx) {
+///COMPOSITE CHART
+
+function overall_vs_potential(ndx, data) {
   var overall_dim = ndx.dimension(dc.pluck("Name"));
   var overall = overall_dim.group().reduceSum(d => {
     console.log("Overall", d.Overall);
@@ -203,35 +188,7 @@ function show_stacked_chart(ndx) {
   stackedChart.margins().right = 100;
 }
 
-function show_stacked_chart(ndx) {
-  var name_dim = ndx.dimension(dc.pluck("Club"));
-  var releaseGroup = name_dim
-    .group()
-    .reduceSum(d => parseCurrency(d, "Release Clause"));
-  var valueGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Value"));
-  var wageGroup = name_dim.group().reduceSum(d => parseCurrency(d, "Wage"));
-  var stackedChart = dc.barChart("#stacked-chart");
-  stackedChart
-    .width(990)
-    .height(500)
-    .dimension(name_dim)
-    .group(releaseGroup, "Release Clause")
-    .stack(valueGroup, "Value")
-    .stack(wageGroup, "Wage")
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .legend(
-      dc
-        .legend()
-        .x(50)
-        .y(0)
-        .itemHeight(15)
-        .gap(5)
-    );
-  stackedChart.margins().right = 100;
-}
-
-// Wage 
+// Utils
 function parseCurrency(d, variable = "Wage") {
   const value = d[variable].replace(/€/, "");
   let multiplier = 0;
